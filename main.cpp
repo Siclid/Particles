de9e5fc74ba73a43aa3b31e33c6fecbfd5229da2
tcoldx <t.tredis@gmail.com>
2024-12-01 19:49:52 -0800

    random change

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "Engine.h"
+// change to repo Tredis/Joshua
+int main()
+{
+	// Declare an instance of Engine
+	Engine engine;
+	// Start the engine
+	engine.run();
+	// Quit in the usual way when the engine is stopped
+	return 0;
+}
